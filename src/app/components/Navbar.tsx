3972f099701eb140f2bc311d2abfe4505a393f16
ssatriya <ssatriya@gmail.com>
2023-04-26 23:12:54 +0700

    Added default value for dark mode

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -56,6 +56,9 @@ const Navbar = () => {
     if (storedDarkThemeState && storedDarkThemeState === "off") {
       setCheckedValue(false);
     }
+    if(!storedDarkThemeState) {
+      setTheme('light')
+    }
   }, []);
 
   return (
